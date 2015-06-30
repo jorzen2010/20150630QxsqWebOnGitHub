8697c26b129f2f45cfe921189aebef40d04316b9
jorzen2010 <jorzen2010@163.com>
2015-06-30 20:33:54 +0800

    新建一个mvc项目
    新建一个mvc项目

---
+++ QxsqWebOnGitHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QxsqWebOnGitHub.Startup))]
+namespace QxsqWebOnGitHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
